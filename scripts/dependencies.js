89a5b88af40e45608ff5adea02c869f6e17df51a
jpcesar <jpcesar@gmail.com>
2020-05-13 02:01:07 +0100

    Update dependencies.js

--- scripts/dependencies.js
+++ scripts/dependencies.js
@@ -1,6 +1,1 @@
-module.exports = function (context) {
-    var shell = context.requireCordovaModule('lodash');
-
-    shell.cd(context.opts.plugin.dir);
-    shell.exec('npm install');
-};
+const lodash = require('lodash');
